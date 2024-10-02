8eeaa27cfa3b73e52f72f4ef7f70d600c78a6abe
coola <cool.artyukov@mail.ru>
2024-10-02 09:05:55 +0300

    add realization Filtration

--- src/JavaCollection/CountOfElemtnts.java
+++ src/JavaCollection/CountOfElemtnts.java
@@ -1,5 +1,16 @@
 package JavaCollection;
 
-public class CountOfElemtnts {
+import java.util.HashMap;
+import java.util.Map;
 
+public class CountOfElemtnts {
+    public static <T> Map<T, Integer> countElements(T[] array) {
+        Map<T, Integer> elementCountMap = new HashMap<>();
+
+        for (T element : array) {
+            elementCountMap.put(element, elementCountMap.getOrDefault(element, 0) + 1);
+        }
+
+        return elementCountMap;
+    }
 }
